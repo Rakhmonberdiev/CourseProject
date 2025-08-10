ad20641a7ca710588665162b794a83e70ffb176a
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2025-08-10 13:58:31 +0500

    Revert "feat(domain): добавлены основные модели для Inventory Management"
    This reverts commit 60d58211f6f0d4ed6f6649f93adf97fa3dadc01e.

--- Domain/Tags/Tag.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Tags;
-
-public sealed class Tag
-{
-    public Guid Id { get; set; }
-    public string Value { get; set; } = null!; // normalized lowercase
-
-    public ICollection<InventoryTag> Inventories { get; set; } = new List<InventoryTag>();
-}
