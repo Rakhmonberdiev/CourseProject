56236a495ad87c228ec6a604fb6f3ace609c41f4
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2025-08-10 14:00:28 +0500

    Reapply "feat(domain): добавлены основные модели для Inventory Management"
    This reverts commit ad20641a7ca710588665162b794a83e70ffb176a.

---
+++ Domain/Common/FieldType.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Domain.Common
+{
+
+    public enum FieldType
+    {
+        SingleLineText = 1,
+        MultiLineText = 2,
+        Numeric = 3,
+        DocumentLink = 4,
+        Boolean = 5
+    }
+}
---
+++ Domain/Inventories/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Items;
+using Domain.Tags;
+using Domain.Users;
+
+namespace Domain.Inventories
+{
+    public sealed class Inventory
+    {
+        public Guid Id { get; set; }
+
+        public Guid OwnerId { get; set; }
+        public AppUser Owner { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string? DescriptionMarkdown { get; set; }
+        public string? Category { get; set; }
+        public string? ImageUrl { get; set; }
+        public bool IsPublicWritable { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public uint Xmin { get; private set; }
+        public ICollection<InventoryAccess> AccessList { get; set; } = new List<InventoryAccess>();
+        public ICollection<InventoryField> Fields { get; set; } = new List<InventoryField>();
+        public CustomIdFormat? CustomIdFormat { get; set; }
+        public ICollection<InventoryItem> Items { get; set; } = new List<InventoryItem>();
+        public ICollection<DiscussionPost> DiscussionPosts { get; set; } = new List<DiscussionPost>();
+        public ICollection<InventoryTag> Tags { get; set; } = new List<InventoryTag>();
+    }
+}
